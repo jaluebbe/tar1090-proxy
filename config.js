9784c7dd52dd8dc34c691e5a256bdcd247730e67
Jannis Lübbe <jannis@jl82.de>
2024-11-15 18:18:31 +0100

    added config.js example.

---
+++ config.js
@@ -0,0 +1,8 @@
+"use strict";
+BingMapsAPIKey = null;
+MapboxAPIKey = null;
+DefaultCenterLat = 50.883;
+DefaultCenterLon = 4.431;
+DefaultZoomLvl   = 8;
+useRouteAPI = true;
+routeApiUrl = "https://api.adsb.lol/api/0/routeset";
